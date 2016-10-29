1518efdc5f07a28a9261ef4232e00cd1aad9c6d7
borepstein <borepstein@gmail.com>
2016-10-29 15:56:28 -0400

    Code stub - initial code

--- include/DAMInstance.h
+++ include/DAMInstance.h
@@ -1,6 +1,31 @@
 #ifndef _DAMINSTANCE_H_INCLUDED
 #define _DAMINSTANCE_H_INCLUDED
 
+/**
+instanceRunStatus values 
+ */
+#define INSTANCE_RUN_STATUS_OK 0
+#define INSTANCE_RUN_STATUS_STOP 1
 
+class DAMInstance{
+ public:
+  DAMInstance();
+  ~DAMInstance();
+  byte getInstanceRunStatus(void);
+  void updateStatus(void);
+  
+ protected:
 
-#endif
+ private:
+  byte instanceRunStatus;
+}
+
+DAMInstance::DAMInstance(void){
+  instnaceRunStatus = INSTANCE_RUN_STATUS_O;
+}
+
+byte DAMInstance::getInstanceRunStatus(void){
+  return( instanceRunStatus );
+}
+
+#endif /* _DAMINSTANCE_H_INCLUDE */
