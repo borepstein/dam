bcf1f810f44dadce0176992636a85721a0d36f78
borepstein <borepstein@gmail.com>
2016-10-29 15:55:55 -0400

    Code stub

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,14 @@
-#include <stdin.h>;
+#include <stdio.h>
+#include <DAMInstance.h>
 
+int main(int argc, char **argv)
+{
+  private DAMInstance runInstance;
+
+  runInstance = new DAMInstance();
+
+  while (! runInstance.getRunStatus())
+    runInstance.updateStatus();
+
+  runInstance.~DAMInstance();
+}
