5e951e878bc8d527257e117fa15491e85cd19011
borepstein <borepstein@gmail.com>
2021-10-25 15:55:50 -0400

    First pass - buildable code.

---
+++ src/test/test_dam_core.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+import "dam_core"
+
+func main(){
+    content := make(dam_core.UnivDataContent)
+    content["de_type"] = (byte)(2)
+    content["de_content"] = (byte)(135)
+    fmt.Printf("content=%s\n", content)
+    var de dam_core.UnivDataContent
+    (&de).InitDataEntry(&content)
+    fmt.Printf("de=%s\n", de)
+}
